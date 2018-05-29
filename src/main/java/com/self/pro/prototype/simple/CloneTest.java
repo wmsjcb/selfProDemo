0600894d054d9674a5e4aff6cd2c7c36f2de3766
chenbinbin3 <chenbinbin1@jd.com>
2018-05-29 19:08:27 +0800

    测试简单原型模式

---
+++ src/main/java/com/self/pro/prototype/simple/CloneTest.java
@@ -0,0 +1,40 @@
+package com.self.pro.prototype.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by chenbinbin1 on 2018/5/29.
+ */
+public class CloneTest {
+    public static void main(String[] args) {
+        Prototype p = new Prototype();
+        p.setName("1234");
+        List<String> ls = new ArrayList<String>();
+        ls.add("111");
+        ls.add("22");
+        p.setLs(ls);
+        System.out.println(p+" "+p.getName());
+        try {
+            Prototype p2= (Prototype) p.clone();
+            System.out.println(p2+" "+p2.getName()+"  "+p2.getLs());
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+
+//        CloneTarget c= new CloneTarget();
+//         c.setName("wmsjcb");
+//         c.setCloneTarget(new CloneTarget());
+//        System.out.println(c.getCloneTarget());
+//
+//        try {
+//            CloneTarget c2 = (CloneTarget) c.clone();
+//            System.out.println(c2.getCloneTarget()+" "+c2.getName());
+//        } catch (CloneNotSupportedException e) {
+//            e.printStackTrace();
+//        }
+
+
+    }
+}
--- src/main/java/com/self/pro/prototype/simple/Prototype.java
+++ src/main/java/com/self/pro/prototype/simple/Prototype.java
@@ -1,9 +1,21 @@
 package com.self.pro.prototype.simple;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Prototype implements Cloneable{
     private String name;
 
+    private List<String> ls = new ArrayList<>();
+
     private  CloneTarget cloneTarget=null;
+
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
     public String getName() {
         return name;
     }
@@ -18,5 +30,13 @@ public class Prototype implements Cloneable{
 
     public void setCloneTarget(CloneTarget cloneTarget) {
         this.cloneTarget = cloneTarget;
+    }
+
+    public List<String> getLs() {
+        return ls;
+    }
+
+    public void setLs(List<String> ls) {
+        this.ls = ls;
     }
 }
